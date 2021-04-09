fe89706c9d07ce49f62ffa8ce8c9012d22329b42
akinghill <Austinkinghill@gmail.com>
2021-04-09 10:02:00 -0700

    fixes

--- App.js
+++ App.js
@@ -1,8 +1,24 @@
 import * as React from 'react';
-import {Text, View} from 'react-native';
-import Nav from './src/Navigation/navigator';
+import {Text, View, StyleSheet} from 'react-native';
+
+// import Nav from './src/Navigation/navigator';
 
 function App() {
-  return <Nav />;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Add your name to the list below :) </Text>
+      <Text style={styles.text}>- Austin Hill</Text>
+    </View>
+  );
 }
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
 export default App;
